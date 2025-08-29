c7687480f9f16db0c1db1d385ed03f1628bdb01f
sofowest <leandrogomezmdev@gmail.com>
2025-08-29 08:31:35 -0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,16 @@ function Home() {
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1 className="text-4xl font-bold">Leandro Gómez - Developer</h1>
       <p className="mt-4 text-lg">I'm a passionate developer.</p>
+
+      <h2>My stack</h2>
+      <ul className="list-disc list-inside">
+        <li>JavaScript</li>
+        <li>React</li>
+        <li>Node.js</li>
+      </ul>
+
+      <h2>Contact Me</h2>
+      <p>If you'd like to get in touch, feel free to reach out!</p>
     </div>
   );
 }
