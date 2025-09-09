2d2549a241362446fc198418fae2b254ccd700b6
Leandro Gómez <leandrogomezmdev@gmail.com>
2025-09-08 21:47:24 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import './global.css'
-import Header from "./components/layout/header.jsx";
 import Home from './pages/Home.jsx'
 
 
@@ -8,8 +7,7 @@ function App() {
 
   return (
     <>
-      <Header />
-      <main className='bg-[#1e2358] min-h-screen flex items-center justify-center'>
+      <main className='min-h-screen flex items-center justify-center bg-[var(--dark-purple)]'>
         <Home />
       </main>
     </>
