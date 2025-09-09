ccec0473be803d8d75d81d3f6a7efd880992326b
Leandro Gómez <leandrogomezmdev@gmail.com>
2025-09-09 01:39:47 -0300

    Refine SocialIcon and Home page styles
    Updated SocialIcon to use responsive icon sizing and enhanced hover effects. Improved Home page layout with responsive widths and added text shadow styling for headings and alias.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,11 +4,11 @@ import SocialIcon from "../components/ui/SocialIcon.tsx";
 
 function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2 text-white overflow-hidden w-full bg-[var(--bckg-gray)] shadow-2xl">
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 text-white overflow-hidden w-full bg-[var(--bckg-gray)] shadow-2xl lg:w-[800px] xl:w-[1080px] 2xl:w-[1280px]">
 
-      <article className="flex justify-center items-center text-center h-[100vh] flex-col">
-        <h1 className="p-2 text-4xl font-extrabold">Hey, I'm <span className="text-[var(--light-purple)]">Leandro</span></h1>
-        <h2 className="p-0 text-2xl font-medium">( alias <span className="text-[var(--light-purple)] font-bold">Sofo</span> )</h2>
+      <article className="flex justify-center items-center text-center h-[100vh] flex-col text-[var(--extra-light-purple)]">
+        <h1 className="p-2 text-4xl font-extrabold"><span className="text-shadow-light">Hey, I'm </span><span className="text-[var(--light-purple)] text-shadow-name">Leandro</span></h1>
+        <h2 className="p-0 text-2xl font-medium text-shadow-light">( alias <span className="text-[var(--light-purple)] font-bold text-shadow-name">Sofo</span> )</h2>
         <p className="p-2 py-10 font-medium">I build modern, <span className="text-[var(--light-purple)] font-bold">mobile-first</span><br /> websites that connect ideas<br /> <span className="text-[var(--light-purple)] font-bold">with people.</span></p>
         <section className="flex space-x-4">
           <SocialIcon url="https://github.com/sofowest" icon="github" />
