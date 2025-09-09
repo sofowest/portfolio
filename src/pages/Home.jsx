04c94ec511b8369b325a58fad9ce017f16a447a9
Leandro Gómez <leandrogomezmdev@gmail.com>
2025-09-08 21:51:23 -0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,9 +6,9 @@ function Home() {
     <div className="flex flex-col items-center justify-center min-h-screen py-2 text-white overflow-hidden w-full bg-[var(--bckg-gray)] shadow-2xl">
 
       <article className="flex justify-center items-center text-center h-[100vh] flex-col">
-        <h1 className="p-2 text-3xl">Hey, I'm <span className="text-[var(--light-purple)]">Leandro</span></h1>
-        <h2 className="p-2 text-2xl">(Alias <span>Sofo</span>)</h2>
-        <p className="p-2">I build modern, <span>mobile-first</span><br /> websites that connect ideas<br /> <span>with people.</span></p>
+        <h1 className="p-2 text-4xl font-extrabold">Hey, I'm <span className="text-[var(--light-purple)]">Leandro</span></h1>
+        <h2 className="p-2 text-2xl font-medium">( alias <span className="text-[var(--light-purple)] font-bold">Sofo</span> )</h2>
+        <p className="p-2 font-medium">I build modern, <span className="text-[var(--light-purple)] font-bold">mobile-first</span><br /> websites that connect ideas<br /> <span className="text-[var(--light-purple)] font-bold">with people.</span></p>
         <section className="p-4">
 
         </section>
