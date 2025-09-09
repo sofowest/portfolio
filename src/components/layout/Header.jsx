8fdc70d5fc1199e5ad4af0a309524d85e2b192a4
Leandro Gómez <leandrogomezmdev@gmail.com>
2025-09-08 21:20:43 -0300

    Delete Header.jsx

--- src/components/layout/Header.jsx
+++
@@ -1,7 +0,0 @@
-export default function Header() {
-  return (
-    <header className="w-full p-4 bg-[#1e2358] text-white shadow-md fixed top-0 left-0 z-10">
-      <h1 className="text-3xl font-bold text-center">Mi Portafolio</h1>
-    </header>
-  );
-}
