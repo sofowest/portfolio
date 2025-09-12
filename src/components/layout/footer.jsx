0836d7fc00845dc346d86a31ed30fe16c7611060
Leandro Gómez <leandrogomezmdev@gmail.com>
2025-09-12 20:45:32 -0300

    Update footer.jsx

--- src/components/layout/footer.jsx
+++ src/components/layout/footer.jsx
@@ -6,11 +6,11 @@ function Footer() {
           Made with <span className="text-[var(--light-purple)]">💜</span> and so much coffee by Leandro Gómez.
         </p>
         <p className="text-sm">Credits for 
-          <a href="https://www.youtube.com/@midudev" target="_blank" rel="noopener noreferrer">Midudev</a>, 
-          <a href="https://www.youtube.com/@BettaTech" target="_blank" rel="noopener noreferrer">BettaTech</a>, 
-          <a href="https://www.youtube.com/@mouredev" target="_blank" rel="noopener noreferrer">MoureDev</a>, 
-          <a href="https://www.youtube.com/@goncypozzo" target="_blank" rel="noopener noreferrer">Goncy</a> and 
-          <a href="https://www.youtube.com/@hdeleonnet" target="_blank" rel="noopener noreferrer">HdeLeon</a>
+          <a className="font-bold hover:text-[var(--extra-light-purple)] " href="https://www.youtube.com/@midudev" target="_blank" rel="noopener noreferrer">Midudev</a>, 
+          <a className="font-bold hover:text-[var(--extra-light-purple)] " href="https://www.youtube.com/@BettaTech" target="_blank" rel="noopener noreferrer">BettaTech</a>, 
+          <a className="font-bold hover:text-[var(--extra-light-purple)] " href="https://www.youtube.com/@mouredev" target="_blank" rel="noopener noreferrer">MoureDev</a>, 
+          <a className="font-bold hover:text-[var(--extra-light-purple)] " href="https://www.youtube.com/@goncypozzo" target="_blank" rel="noopener noreferrer">Goncy</a> and 
+          <a className="font-bold hover:text-[var(--extra-light-purple)] " href="https://www.youtube.com/@hdeleonnet" target="_blank" rel="noopener noreferrer">HdeLeon</a>
         </p>
       </div>
     </footer>
