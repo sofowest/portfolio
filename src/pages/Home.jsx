b425a6ee6c493bc7d6d1cf3ca8155bcd85edaaaa
Leandro Gómez <leandrogomezmdev@gmail.com>
2025-09-11 21:46:54 -0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -70,8 +70,8 @@ function Home({ onFlickerBg, setIsNameFlicker }) {
           <SocialIcon url="https://wa.me/59891066507" icon="whatsapp" />
           <SocialIcon url="mailto:leandrogomezmdev@gmail.com" icon="email" />
         </section>
-        <section className="z-10 absolute bottom-0 text-[var(--light-purple)] w-15 h-20 bg-[var(--dark-purple)] rounded-t-full flex items-center justify-center">
-          <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="rounded-full w-13 h-13 animate-bounce mt-2">
+        <section className="z-10 absolute bottom-0 text-[var(--light-purple)] w-12 h-17 bg-[var(--dark-purple)] rounded-t-full flex items-center justify-center">
+          <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="rounded-full w-10 h-10 animate-bounce mt-2">
             <path d="M9 13L12 16M12 16L15 13M12 16V8M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="text-shadow-2xs text-shadow-amber-50"></path>
           </svg>
         </section>
