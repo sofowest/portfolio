ab7afd4875afa7eef661c15fb8fb537ff55c3d81
Leandro Gómez <leandrogomezmdev@gmail.com>
2025-09-11 21:31:40 -0300

    Update Home page layout and About Me section
    Improved the Home page by adjusting layout styles, adding a bouncing arrow SVG, and enhancing the About Me section with updated styling and English text. The changes provide a more visually appealing and informative introduction for users.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -54,7 +54,7 @@ function Home({ onFlickerBg, setIsNameFlicker }) {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2 overflow-hidden w-full bg-[var(--bckg-gray)] lg:shadow-[0px_0px_100px_5px_#522b82] lg:w-[800px] xl:w-[1080px]">
 
-      <article className="flex justify-center items-center text-center h-[100vh] flex-col">
+      <article className="flex justify-center items-center text-center h-[100vh] flex-col relative">
         <h1 className="p-2 text-5xl font-extrabold leading-15 lg:text-5xl lg:p-3 xl:text-6xl">
           <span>Hey, I'm <br className="md:hidden" /></span>
           <span className={"text-[var(--light-purple)] " + (flicker ? "flicker" : "")}>{displayedName}
@@ -70,15 +70,22 @@ function Home({ onFlickerBg, setIsNameFlicker }) {
           <SocialIcon url="https://wa.me/59891066507" icon="whatsapp" />
           <SocialIcon url="mailto:leandrogomezmdev@gmail.com" icon="email" />
         </section>
+        <section className="z-10 absolute bottom-0 text-[var(--light-purple)] w-15 h-23 bg-[var(--dark-purple)] rounded-t-full flex items-center justify-center">
+          <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="rounded-full w-13 h-13 animate-bounce mt-2">
+            <path d="M9 13L12 16M12 16L15 13M12 16V8M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="text-shadow-2xs text-shadow-amber-50"></path>
+          </svg>
+        </section>
       </article>
 
-      <article className="h-[100vh] p-5 flex flex-col mt-10 gap-7 items-center justify-center">
-        <h2 className="text-4xl font-extrabold text-center">About Me</h2>
+      <article className="h-[110vh] w-[100vw] flex flex-col gap-10 items-center justify-center bg-[var(--dark-purple)] back">
+        <h2 className="text-5xl font-extrabold text-center">About Me</h2>
         <section className="justify-center items-center flex">
-          <img src="./imgs/profile.jpg" alt="foto de " className="w-[70vw] max-w-[380px] h-auto md:w-[300px] rounded-2xl" />
+          <img src="./imgs/profile.jpg" alt="foto de " className="w-[90vw] h-auto md:w-[300px] rounded-2xl" />
         </section>
-        <section className="w-[70vw] text-left mb-10">
-          <p className="text-md">Empecé a programar a los 7 años y desde entonces he estado en un viaje de aprendizaje constante. He trabajado con una variedad de tecnologías y lenguajes de programación, y siempre estoy buscando nuevas formas de mejorar mis habilidades. Me podrás encontrar siempre programando algo nuevo, o mejorandolo, mientras escucho mi playlist de boombap.</p>
+        <section className="w-[90vw] text-left mb-10 gap-4 flex flex-col">
+          {/* <p className="text-md">Empecé a programar a los 7 años y desde entonces he estado en un viaje de aprendizaje constante. He trabajado con una variedad de tecnologías y lenguajes de programación, y siempre estoy buscando nuevas formas de mejorar mis habilidades. Me podrás encontrar siempre programando algo nuevo, o mejorandolo, mientras escucho mi playlist de boombap o debates de temas interesantes de fondo.</p> */}
+          <p className="text-base">At the age of 7, I started programming and have been on a constant learning journey ever since.</p>
+          <p className="text-base">I have worked with a variety of technologies and programming languages, and I am always looking for new ways to improve my skills. You can always find me programming something new or improving it while listening to my boombap playlist or debates on interesting topics in the background.</p>
         </section>
       </article>
 
