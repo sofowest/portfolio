9d3932d0c76b13c4506acd71caa612276085af18
Leandro Gómez <leandrogomezmdev@gmail.com>
2025-09-13 21:45:57 -0300

    Update Project.tsx

--- src/components/ui/Project.tsx
+++ src/components/ui/Project.tsx
@@ -4,12 +4,12 @@ interface ProjectProps {
   imageUrl: string;
 }
 
-const baseClasses = "bg-[#1e2358] text-white rounded-lg  w-full my-2 shadow-lg shadow-[#2b3280] h-100";
+const baseClasses = "bg-[#1e2358] text-white rounded-xl  w-full my-2 shadow-lg shadow-[#2b3280] h-100 p-10 flex flex-col md:flex-row gap-4";
 function Project ({title, description, imageUrl}: ProjectProps) {
   return (
     <div className={baseClasses}>
       <section className="flex justify-center">
-        <img src={imageUrl} alt={`${title} project image`} className="w-full h-full rounded-lg"/>
+        <img src={imageUrl} alt={`${title} project image`} className="w-[90%] h-full rounded-lg"/>
       </section>
       <section className="mt-4">
         <h3 className="text-2xl font-bold">{title}</h3>
